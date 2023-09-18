d3f7244373a609ed806207b43a69f3998d3be2f4
Alícea P. de Lima Rocha <alicealrocha@outlook.com>
2023-09-18 17:17:19 -0300

    centralizando botão

--- api_proton/src/pages/myProjects/MyProjets.tsx
+++ api_proton/src/pages/myProjects/MyProjets.tsx
@@ -3,15 +3,14 @@ import { Box, Paper, Typography, Button, Grid,  } from "@mui/material";
 export const MyProjects = () => {
   
     return (
-        <Box display="flex" alignItems="center" justifyContent="center" maxHeight="100vh"
-        flexDirection="column" sx={{gap:3}}>
+        <Box display="flex" alignItems="center" flexDirection="column" sx={{gap:3}} justifyContent="center">
 
             <Paper sx={{mt:3, padding:3, borderRadius: 5,  width: '1000px', height: '480px', gap: 3}}>
 
                 <Typography variant="h4" color="primary">Seus Projetos</Typography>
 
-                <Grid item>
-                    <Button className="botaolindao " variant="contained" disableElevation > + Criar Projeto </Button>
+                <Grid item container justifyContent="center" alignItems="center" style={{ height: '60vh' }}>
+                    <Button variant="contained" disableElevation > + Criar Projeto </Button>
                 </Grid>
 
             </Paper>
