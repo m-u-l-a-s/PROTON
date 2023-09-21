da30a63a58ca912449819ca0442ae75afb982134
AmendoaM <delakyu@gmail.com>
2023-09-21 09:15:39 -0300

    Alteraçoões no calendário da página de NovaEtapa

--- api_proton/src/pages/novaEtapa/NovaEtapa.tsx
+++ api_proton/src/pages/novaEtapa/NovaEtapa.tsx
@@ -15,23 +15,12 @@ import {
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { color } from "@mui/system";
-
+import "./NovaEtapa.css";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import FirstComponent from "./calendario";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 // import { ProjectSteps } from "../../shered/components/project_steps/ProjectSteps";
-
-
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
 
 export const NovaEtapa = () => {
   const theme = useTheme();
@@ -59,104 +48,69 @@ export const NovaEtapa = () => {
             </Grid>
 
             <div style={{ display: "flex", flexDirection: "column" }}>
+              <div style={{ display: "flex", flexDirection: "row", marginBottom: "20px" }}>
+                <TextField
+                  id="standard-multiline-static-criador"
+                  label="Criador:"
+                  variant="standard"
+                  sx={{ width: "20vw", borderBottom: "none", marginRight: "16px" }}
+                />
+                <TextField
+                  id="standard-multiline-static-responsavel"
+                  label="Responsável:*"
+                  variant="standard"
+                  sx={{ width: "20vw", borderBottom: "none", marginRight: "16px" }}
+                />
+                <TextField
+                  id="standard-multiline-static-responsavel"
+                  label="Envolvidos:"
+                  variant="standard"
+                  sx={{ width: "20vw", borderBottom: "none" }}
+                />
+              </div>
 
-  <div style={{ display: "flex", flexDirection: "row", marginBottom: "20px" }}>
-    <TextField
-      id="standard-multiline-static-criador"
-      label="Criador:"
-      variant="standard"
-      sx={{ width: "20vw", borderBottom: "none", marginRight: "16px" }}
-    />
-    <TextField
-      id="standard-multiline-static-responsavel"
-      label="Responsável:*"
-      variant="standard"
-      sx={{ width: "20vw", borderBottom: "none", marginRight: "16px" }}
-    />
-    <TextField
-      id="standard-multiline-static-responsavel"
-      label="Envolvidos:"
-      variant="standard"
-      sx={{ width: "20vw", borderBottom: "none" }}
-    />
-  </div>
+              <div style={{ display: "flex", alignItems: "center", marginBottom: "15px" }}>
+                <span style={{ marginRight: "10px" }} className="span">
+                  Checklist de documentos obrigatórios:
+                </span>
+                <Button
+                  variant="contained"
+                  size = "small"
+                  style={{
+                    borderRadius: "50%",
+                    borderColor: "#373737",
+                    width: "35px",
+                    height: "35px",
+                    minWidth: "35px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#616161",
+                    border: "2px solid white",
+                    cursor: "pointer",
+                  }}
+                >
+                  <AddIcon style={{ fontSize: "20px", color: "#373737" }} />
+                </Button>
+              </div>
 
-  
-  
-<div style={{ display: 'flex', alignItems: 'center',  marginBottom: "15px" }}>
-  <span style={{ marginRight: '10px'}}>Checklist de documentos obrigatórios:</span>
-  <Button
-    variant="contained"
-    style={{ borderRadius: '50%', width: '35px', height: '35px', minWidth: '35px', display: 'flex', alignItems: 'center',
-     justifyContent: 'center', backgroundColor: '#616161', border: '2px solid white', cursor: 'pointer', }}
-  >
-    <AddIcon style={{ fontSize: '20px', color: "white" }} />
-    
-  </Button>
-</div>
+              <div style={{ display: "flex", alignItems: "center", marginBottom: "15px" }}>
+                <span style={{ marginRight: "10px", fontFamily: "Poppins, sans-serif" }}>Prazo de conclusão:*</span>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker />
+                </LocalizationProvider>
+              </div>
 
+              {/* <div style={ {marginBottom: "10px"}}><p>Prazo de conclusão:*</p></div> */}
 
+              <div style={{ color: "black" }} className="descricao">
+                <p>Descrição:</p>
+              </div>
 
-<div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-  <span style={{ marginRight: '10px', fontFamily: 'Poppins, sans-serif' }}>Prazo de conclusão:*</span>
-  <button
-    style={{
-      width: '35px',
-      height: '35px',
-      backgroundColor: '#616161',
-      border: '2px solid white',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      cursor: 'pointer',
-      borderRadius: '50%'
-    }}
-    onClick={() => {
-      
-      console.log('Botão de calendário clicado!');
-    }}
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
-      fill="white"
-    >
-      <path d="M13 0h2v2h-2zM3 0h2v2H3zm3 0h2v2H6zm3 0h2v2h-2zm3 0h2v2h-2zM0 3h18v14H0z" />
-    </svg>
-  </button>
-</div>
-
-
-
-
-
-
-
-
-
-
-    {/* <div style={ {marginBottom: "10px"}}><p>Prazo de conclusão:*</p></div> */}
-
-
-    <div style={{color: "black"}}><p>Descrição:</p></div>
-    
-
-
-
-<div style={{ display: "flex", flexDirection: "row", borderColor: "white" }}>
-  <TextField
-    id="standard-multiline-static-responsavel"
-    label=""
-    sx={{ width: "50vw" }}
-  />
-</div>
-  
-</div>
-
-
-
+              <div style={{ display: "flex", flexDirection: "row", borderColor: "white" }}>
+                <TextField id="standard-multiline-static-responsavel" label="" sx={{ width: "50vw" }} />
+              </div>
+            </div>
 
             <Grid item>
               <Box display="flex" flexDirection="row" alignItems="flex-end" sx={{ gap: 80, marginTop: 4 }}>
