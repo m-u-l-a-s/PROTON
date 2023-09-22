7e030da7d658d9d3afa54c11a6537d5b02a18b94
AmendoaM <delakyu@gmail.com>
2023-09-21 21:21:00 -0300

    Alterações na página de etapas

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-const Pool = require("pg").Pool
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    password: 'fatec',
-    database: 'proton'
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "mulinhas44",
+  database: "proton",
 });
 
-module.exports = pool
+module.exports = pool;
