4b91c7e77b1c3c7e00e48bf8048c08dc7b0e1e8a
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-09-22 20:41:23 -0300

    novo Projeto mexendo no front

--- api_proton/src/pages/novoProjeto/NovoProjeto.tsx
+++ api_proton/src/pages/novoProjeto/NovoProjeto.tsx
@@ -1,23 +1,22 @@
-import { useTheme } from "@emotion/react";
-import { Box, Button, Divider, Grid, ListItem, ListItemButton, ListItemText, Paper, TextField, Typography } from "@mui/material";
+
+import { Box, Button, Grid, IconButton, Paper, TextField, Typography } from "@mui/material";
 
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import { Link } from 'react-router-dom';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 
 
 
-import {Steps} from "./Steps";
-import React, { useEffect, useState } from 'react';
 
-    
+
 
 
 
 export const NovoProjeto = () => {
 
-   
+
 
     return (
         // <><Grid display="flex" alignItems="center" justifyContent="center" maxHeight="100vh" sx={{gap:3}}>
@@ -25,51 +24,92 @@ export const NovoProjeto = () => {
         <Box display="flex" alignItems="center" justifyContent="center" maxHeight="100vh"
             flexDirection="column" sx={{ gap: 3 }}>
 
-            <Paper sx={{ mt: 3, padding: 3, borderRadius: 5, width: '1000px', height: '480px', gap: 3 }}>
+            <Paper sx={{
+                mt: 3,
+                padding: 3,
+                borderRadius: 5,
+                width: 'fit-content',
+                height: 'fit-content',
+                gap: 2,
+                display: "flex",
+                flexDirection: "column",
+                flexWrap: "wrap",
+                maxWidth: '70%',
+                maxHeight: '50%',
+                marginTop: '5%',
 
-                <Typography variant="h4" color="primary">Novo Processo</Typography>
+                // overflow: "scroll",
+                // overflowX: "hidden"
+
+
+            }}>
+
+                <Grid >
+
+                    <Grid item marginLeft={"-0.5rem"}>
+
+                        <IconButton className="meuBotao" component={Link} to="/MyProjects">
+                            <ArrowBackRoundedIcon />
+                        </IconButton>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography variant="h4" color="primary">Novo Processo</Typography>
+                    </Grid>
+
+                </Grid>
+
+                <Grid>
+                    <Box alignItems="center" maxHeight="100vh" flexDirection="column">
+
+
+                        <Box display="flex" flexDirection="column" sx={{ gap: 2 }} >
+
+
+                            <Grid item position="relative">
+                                <TextField id="nome-projeto" label="Nome:" variant="standard" sx={{ width: "65vw" }} />
+
+                            </Grid>
+
+                            <Grid item position="relative">
+
+                                <TextField id="standard-multiline-static" label="Descrição" multiline rows={4}
+                                    variant="standard" sx={{ width: "65vw" }} />
+
+                            </Grid>
 
 
 
-                <Box display="flex" alignItems="center" maxHeight="100vh" flexDirection="column">
 
 
-                    <Box display="flex" flexDirection="column" sx={{ gap: 2 }} >
+                        </Box>
 
 
-                        <Grid item>
-                            <TextField id="nome-projeto" label="Nome:" variant="standard" sx={{ width: "65vw" }} />
 
-                        </Grid>
 
-                        <Grid item>
+                        <Box display="flex" flexDirection="row" alignItems="flex-end" sx={{ gap: "36rem", marginTop: "2rem" }}>
 
-                            <TextField id="standard-multiline-static" label="Descrição" multiline rows={2}
-                                variant="standard" sx={{ width: "65vw" }} />
 
-                        </Grid>
+                            <Grid item >
 
-                       
+                                <Button variant="contained" startIcon={<DeleteIcon />} sx={{ background: "#292A2D", color: "white" }}>Descartar</Button>
 
-                        
+                            </Grid>
+
+                            <Grid item >
+
+                                <Button variant="contained" startIcon={<AddIcon />} component={Link} to="/visualizarProjeto">Criar Processo</Button>
+
+                            </Grid>
+
+
+                        </Box>
+
+
+
 
                     </Box>
-
-
-
-
-                    <Box display="flex" flexDirection="row" alignItems="flex-end" sx={{ gap: 72, marginTop: 3 }}>
-
-                        <Button variant="contained" startIcon={<DeleteIcon />} sx={{ background: "#292A2D", color: "white" }}>Descartar</Button>
-                        <Button variant="contained" startIcon={<AddIcon />} component={Link} to="/visualizarProjeto">Criar Processo</Button>
-
-
-                    </Box>
-
-
-
-
-                </Box>
+                </Grid>
 
 
             </Paper>
