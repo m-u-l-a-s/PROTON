9323f17de5efcaeaabe5e4b81fbae55b3055b01f
jonas <jonasalvesbueno@gmail.com>
2023-09-23 17:17:33 -0300

    deletando logo do react e mudando título da aplicação

--- api_proton/src/pages/myProjects/MyProjets.tsx
+++ api_proton/src/pages/myProjects/MyProjets.tsx
@@ -1,48 +1,48 @@
 import { Box, Paper, Typography, Grid, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import { CardProcesso } from "./CardProcesso";
-import { useEffect, useState } from 'react';
-import {useLocation} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export const MyProjects = () => {
-  
+    const [processos, setProcesso] = useState([
+        {
+            processo_id: 1,
+            processo_nome: "Etapa 1",
+            processo_descricao: "Pendente",
+            processo_responsavel_id: 1,
+        },
+        {
+            processo_id: 2,
+            processo_nome: "Etapa 2",
+            processo_descricao: "Concluida",
+            processo_responsavel_id: 1,
+        },
+        {
+            processo_id: 3,
+            processo_nome: "Etapa 3",
+            processo_descricao: "Pendente",
+            processo_responsavel_id: 1,
+        },
+    ]);
 
-    // let processos = [
-    //     { name: "Cards de processos", anexo: "print_github.png", resp: "Joice" },
-    //     { name: "Protótipo figma", anexo: "print.png", resp: "Alicea" },
-    //     { name: "Tela de anexar documentos", anexo: "fodase.png", resp: "Alita" },
-    //     { name: "Cards de processos", anexo: "print_github.png", resp: "Joice" },
-    //     { name: "Cards de processos", anexo: "print_github.png", resp: "Joice" },
-    //     { name: "Cards de processos", anexo: "print_github.png", resp: "Joice" },
-    //     { name: "Cards de processos", anexo: "print_github.png", resp: "Joice" },
-    //     { name: "Cards de processos", anexo: "print_github.png", resp: "Joice" },
-    //     { name: "Cards de processos", anexo: "print_github.png", resp: "Joice" },
-    // ]
+    const get_processos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/get_processos");
+            const jsonData = await response.json();
 
-        const [processos,setProcesso] = useState([
-            {processo_id: 1 ,processo_nome:"Etapa 1", processo_descricao: "Pendente", processo_responsavel_id: 1},
-            {processo_id: 2, processo_nome:"Etapa 2", processo_descricao: "Concluida", processo_responsavel_id: 1},
-            {processo_id: 3, processo_nome:"Etapa 3", processo_descricao: "Pendente", processo_responsavel_id: 1},
-        ])
-    
-        const get_processos = async () => {
-            try {
-                const response = await fetch("http://localhost:5000/get_processos")
-                const jsonData = await response.json()
-                
-                //console.log(jsonData)
-                setProcesso(jsonData)
-                //console.log(processos)
-            } catch (error:any) {
-                console.log(error.message)
-            }
+            //console.log(jsonData)
+            setProcesso(jsonData);
+            //console.log(processos)
+        } catch (error: any) {
+            console.log(error.message);
         }
-    
-        //[] como segundo argumento impede de fazer request 24/7, fazendo apenas uma request
-        useEffect(() => {
-            get_processos();
-        }, [])
+    };
 
+    //[] como segundo argumento impede de fazer request 24/7, fazendo apenas uma request
+    useEffect(() => {
+        get_processos();
+    }, []);
 
     return (
         <Box
@@ -54,56 +54,74 @@ export const MyProjects = () => {
         >
             <Paper
                 sx={{
-                mt: 3,
-                padding: 3,
-                borderRadius: 5,
-                width: 'fit-content',
-                height: 'fit-content',
-                gap: 2,
-                display: "flex",
-                flexDirection: "column",
-                maxWidth: '70%',
-                maxHeight: '50%',
-                marginTop: '5%',
+                    mt: 3,
+                    padding: 3,
+                    borderRadius: 5,
+                    width: "fit-content",
+                    height: "fit-content",
+                    gap: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    maxWidth: "70%",
+                    maxHeight: "50%",
+                    marginTop: "5%",
                 }}
             >
-                <Box display="flex" alignItems="flex-start" maxHeight="100vh" flexDirection="row" sx={{ gap: 70 }}>
-                    
+                <Box
+                    display="flex"
+                    alignItems="flex-start"
+                    maxHeight="100vh"
+                    flexDirection="row"
+                    sx={{ gap: 70 }}
+                >
                     <div className="divBox">
-                        
                         <Typography variant="h4" color="primary">
-                        Seus Processos
+                            Seus Processos
                         </Typography>
-                        
-                        <Button 
-                        component={Link}
-                        to="/NovoProjeto"
-                        variant="contained"
-                        disableElevation
-                        sx={{position: 'absolute', left: '68.9%', marginTop:'-2.6%'}}
+
+                        <Button
+                            component={Link}
+                            to="/NovoProjeto"
+                            variant="contained"
+                            disableElevation
+                            sx={{
+                                position: "absolute",
+                                left: "68.9%",
+                                marginTop: "-2.6%",
+                            }}
                         >
-                        + Criar Processo
+                            + Criar Processo
                         </Button>
-                        
                     </div>
                 </Box>
-      
-                <Box sx={{ height: '50vh', overflowY: 'auto', display: "flex", flexDirection: "column" }}>
 
-                    <Grid item display="flex" flexDirection={"row"} flexWrap={"wrap"} gap={3} marginLeft={3}>
+                <Box
+                    sx={{
+                        height: "50vh",
+                        overflowY: "auto",
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <Grid
+                        item
+                        display="flex"
+                        flexDirection={"row"}
+                        flexWrap={"wrap"}
+                        gap={3}
+                        marginLeft={3}
+                    >
                         {processos.map((processos) => (
-                        <CardProcesso
-                            processoID = {processos.processo_id}
-                            name={processos.processo_nome}
-                            // anexo={processo.anexo}
-                            resp={processos.processo_responsavel_id}
-                        />
+                            <CardProcesso
+                                processoID={processos.processo_id}
+                                name={processos.processo_nome}
+                                // anexo={processo.anexo}
+                                resp={processos.processo_responsavel_id}
+                            />
                         ))}
                     </Grid>
-
                 </Box>
-
-          </Paper>
+            </Paper>
         </Box>
-      );
-}
+    );
+};
