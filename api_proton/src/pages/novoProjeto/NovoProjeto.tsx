a9dfdb9907116936fe14f09e01ea88bc60601512
anxjos <jonassouzafonseca@gmail.com>
2023-09-23 13:07:41 -0300

    Salvando Processo no banco

--- api_proton/src/pages/novoProjeto/NovoProjeto.tsx
+++ api_proton/src/pages/novoProjeto/NovoProjeto.tsx
@@ -1,22 +1,35 @@
-
 import { Box, Button, Grid, IconButton, Paper, TextField, Typography } from "@mui/material";
-
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
-
 import { Link } from 'react-router-dom';
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
-
-
-
-
-
-
-
+import React, { SetStateAction, useState } from "react";
 
 export const NovoProjeto = () => {
 
+    const [processo_responsavel_id, setProcesso_responsavel_id] = useState(1)
+    const [processo_nome, setProcesso_nome] = useState('')
+    const [processo_descricao, setProcesso_descricao] = useState('')
 
+    const inserirProcesso = async (e: any) => {
+        e.preventDefault()
+        try {
+
+            const body = {processo_responsavel_id, processo_nome, processo_descricao }
+            console.log(body)
+
+            const response = await fetch("http://localhost:5000/insert_processo", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            });
+
+            console.log(response)
+
+        } catch (error: any) {
+            console.error(error.message)
+        }
+    }
 
     return (
         // <><Grid display="flex" alignItems="center" justifyContent="center" maxHeight="100vh" sx={{gap:3}}>
@@ -40,14 +53,10 @@ export const NovoProjeto = () => {
 
                 // overflow: "scroll",
                 // overflowX: "hidden"
-
-
             }}>
 
                 <Grid >
-
                     <Grid item marginLeft={"-0.5rem"}>
-
                         <IconButton className="meuBotao" component={Link} to="/MyProjects">
                             <ArrowBackRoundedIcon />
                         </IconButton>
@@ -56,82 +65,42 @@ export const NovoProjeto = () => {
                     <Grid item>
                         <Typography variant="h4" color="primary">Novo Processo</Typography>
                     </Grid>
-
                 </Grid>
 
                 <Grid>
                     <Box alignItems="center" maxHeight="100vh" flexDirection="column">
 
-
                         <Box display="flex" flexDirection="column" sx={{ gap: 2 }} >
 
-
                             <Grid item position="relative">
-                                <TextField id="nome-projeto" label="Nome:" variant="standard" sx={{ width: "65vw" }} />
-
+                                <TextField id="nome-projeto" label="Nome:" variant="standard" sx={{ width: "65vw" }}
+                                    value={processo_nome}
+                                    onChange={(e: { target: { value: SetStateAction<string> } }) => setProcesso_nome(e.target.value)} />
                             </Grid>
 
                             <Grid item position="relative">
-
                                 <TextField id="standard-multiline-static" label="Descrição" multiline rows={4}
-                                    variant="standard" sx={{ width: "65vw" }} />
-
+                                    variant="standard" sx={{ width: "65vw" }}
+                                    value={processo_descricao}
+                                    onChange={(e: { target: { value: SetStateAction<string> } }) => setProcesso_descricao(e.target.value)} />
                             </Grid>
-
-
-
-
-
                         </Box>
-
-
-
 
                         <Box display="flex" flexDirection="row" alignItems="flex-end" sx={{ gap: "36rem", marginTop: "2rem" }}>
 
-
                             <Grid item >
-
                                 <Button variant="contained" startIcon={<DeleteIcon />} sx={{ background: "#292A2D", color: "white" }}>Descartar</Button>
-
                             </Grid>
 
                             <Grid item >
-
-                                <Button variant="contained" startIcon={<AddIcon />} component={Link} to="/visualizarProjeto">Criar Processo</Button>
-
+                                <Button variant="contained" startIcon={<AddIcon />} component={Link} to="/visualizarProjeto" onClick={inserirProcesso}>Criar Processo</Button>
                             </Grid>
-
-
                         </Box>
-
-
-
-
                     </Box>
                 </Grid>
-
-
             </Paper>
-
-
         </Box>
-
         // </Grid></>
-
-
-
-
-
-
-
-
-
-
-
-
     )
-
-
 }
 
