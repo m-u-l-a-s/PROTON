b60eb8b80f5bc5d44f1d3fb87def9c8f751f2e90
Vitor F. Morais <111707785+vmorais111@users.noreply.github.com>
2023-10-24 08:54:19 -0300

    Alteração validar status

--- api_proton/src/control/validarStatusEtapa.ts
+++ api_proton/src/control/validarStatusEtapa.ts
@@ -1,11 +1,22 @@
 export function validarStatus(status:string) {
-    if (status === 'N'){
-        return 'Não iniciado'
-    }
-    else if (status=== 'A') {
-        return 'Em andamento'
-    }
-    else{
-        return 'Concluído'
-    }
-}
+    switch (status) {
+        case 'N':
+            return 'Não iniciado';
+        case 'A':
+            return 'Em andamento';
+        case 'C':
+            return 'Concluído';
+        default:
+            return 'Bn';
+}}
+
+// Código antigo 
+    // if (status === 'N'){
+    //     return 'Não iniciado'
+    // }
+    // else if (status=== 'A') {
+    //     return 'Em andamento'
+    // }
+    // else{
+    //     return 'Concluído'
+    // }
