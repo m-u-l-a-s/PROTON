4d5e83b69bbe189a770944fcaaa605f1bb7a9a16
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-10-25 18:02:00 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/m-u-l-a-s/PROTON""
    This reverts commit 3e66a4ee8945aa8c1f8d7c31991b27745fd850e7.

--- api_proton/src/pages/myProjects/CardProcesso.tsx
+++ api_proton/src/pages/myProjects/CardProcesso.tsx
@@ -1,27 +1,22 @@
 import "./CardProcessos.css";
 import { useNavigate } from "react-router-dom";
 
-export const CardProcesso = (prop: any, ) => {
+export const CardProcesso = (prop: any) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/visualizarProjeto", { state: { id: prop.processoID } });
+  };
 
-    const handleClick = () => {
-                navigate("/visualizarProjeto", {state:{id:prop.processoID}});  
-            }
-    
-    return (
-
-            <div onClick={handleClick} className="cardProcesso">
-                <div className="card">
-                    <h2>{prop.name}</h2>
-                    {/* <p>{prop.anexo}</p> */}
-                    <h3>{prop.resp}</h3>
-                </div>
-            </div>
-    )     
+  return (
+    <>
+      <div onClick={handleClick} className="cardProcesso">
+        <div className="card">
+          <h2>{prop.name}</h2>
+          {/* <p>{prop.anexo}</p> */}
+          <h3>{prop.resp}</h3>
+        </div>
+      </div>
+    </>
+  );
 };
-
-
-
-
-
--- api_proton/src/routes/Routes.tsx
+++ api_proton/src/routes/Routes.tsx
@@ -1,31 +1,36 @@
-
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Home, NovoProjeto, MyProjects, Anexos, NovaEtapa, DetalheEtapa,  } from "../pages";
+import {
+    Home,
+    NovoProjeto,
+    MyProjects,
+    Anexos,
+    NovaEtapa,
+    DetalheEtapa,
+} from "../pages";
 import VisualizarProjeto from "../pages/visualizarProjeto/VisualizarProjeto";
+import FormInput from "../pages/signup/FormInput";
+import Cadastro from "../pages/signup/Cadastro";
 
-
-
-export  const AppRoutes = () =>{
-    
-    return(
+export const AppRoutes = () => {
+    return (
         <Routes>
-            <Route path="/home" element={<Home/>}  />
+            <Route path="/home" element={<Home />} />
 
-            <Route path="/novoProjeto" element={<NovoProjeto/>}/>
-            
-            <Route path="/myProjects" element={<MyProjects/>}/>
+            <Route path="/novoProjeto" element={<NovoProjeto />} />
 
-            <Route path="/visualizarProjeto" element={<VisualizarProjeto/>}/>
+            <Route path="/myProjects" element={<MyProjects />} />
 
-            <Route path="/Anexos" element={<Anexos/>}/>
+            <Route path="/visualizarProjeto" element={<VisualizarProjeto />} />
 
-            <Route path="*" element={<Navigate to="/home"/>}/>
+            <Route path="/Anexos" element={<Anexos />} />
 
-            <Route path="/NovaEtapa" element={<NovaEtapa/>}  />
-            
-            <Route path="/DetalheEtapa" element={<DetalheEtapa/>}/>
+            <Route path="*" element={<Navigate to="/home" />} />
 
+            <Route path="/NovaEtapa" element={<NovaEtapa />} />
+
+            <Route path="/DetalheEtapa" element={<DetalheEtapa />} />
+
+            <Route path="/Cadastro" element={<Cadastro />} />
         </Routes>
-
     );
 };
