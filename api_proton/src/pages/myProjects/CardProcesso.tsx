3e66a4ee8945aa8c1f8d7c31991b27745fd850e7
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-10-25 18:00:46 -0300

    Revert "Merge branch 'main' of https://github.com/m-u-l-a-s/PROTON"
    This reverts commit df9afb6e4874edd1b0928db8c61460f123928af1, reversing
    changes made to e00a2c0e43c7228fd0db44fd993eced77bd19768.

--- api_proton/src/pages/myProjects/CardProcesso.tsx
+++ api_proton/src/pages/myProjects/CardProcesso.tsx
@@ -1,22 +1,27 @@
 import "./CardProcessos.css";
 import { useNavigate } from "react-router-dom";
 
-export const CardProcesso = (prop: any) => {
-  const navigate = useNavigate();
+export const CardProcesso = (prop: any, ) => {
 
-  const handleClick = () => {
-    navigate("/visualizarProjeto", { state: { id: prop.processoID } });
-  };
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <div onClick={handleClick} className="cardProcesso">
-        <div className="card">
-          <h2>{prop.name}</h2>
-          {/* <p>{prop.anexo}</p> */}
-          <h3>{prop.resp}</h3>
-        </div>
-      </div>
-    </>
-  );
+    const handleClick = () => {
+                navigate("/visualizarProjeto", {state:{id:prop.processoID}});  
+            }
+    
+    return (
+
+            <div onClick={handleClick} className="cardProcesso">
+                <div className="card">
+                    <h2>{prop.name}</h2>
+                    {/* <p>{prop.anexo}</p> */}
+                    <h3>{prop.resp}</h3>
+                </div>
+            </div>
+    )     
 };
+
+
+
+
+
