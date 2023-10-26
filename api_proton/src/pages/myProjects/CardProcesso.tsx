b45c640990b6cd2a222743386f4e956ddda7f84b
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-10-26 11:45:28 -0300

    ultimas atualizações da Amanda

--- api_proton/src/pages/myProjects/CardProcesso.tsx
+++ api_proton/src/pages/myProjects/CardProcesso.tsx
@@ -1,25 +1,31 @@
+import React from "react";
 import "./CardProcessos.css";
 import { useNavigate } from "react-router-dom";
+import BarraPrioridade from "./BarraPrioridade";
+
+const verificaStatus = () => {
+    return "CN";
+};
 
 export const CardProcesso = (prop: any) => {
     const navigate = useNavigate();
-  
-    const handleClick = () => {
-      navigate("/visualizarProjeto", { state: { id: prop.processoID } });
-    };
-  
-    return (
-      <>
-        <div onClick={handleClick} className="cardProcesso">
-          <div className="card">
-            <h2>{prop.name}</h2>
-            {/* <p>{prop.anexo}</p> */}
-            <h3>{prop.resp}</h3>
-          </div>
-        </div>
-      </>
-    );
 
-    }
+    const handleClick = () => {
+        navigate("/visualizarProjeto", { state: { id: prop.processoID } });
+    };
+
+    return (
+        <>
+            <div onClick={handleClick} className="cardProcesso">
+                <BarraPrioridade status={verificaStatus()} />
+                <div className="card">
+                    <h2>{prop.name}</h2>
+                    <h3>{prop.resp}</h3>
+                </div>
+            </div>
+        </>
+    );
+};
+
 
 
