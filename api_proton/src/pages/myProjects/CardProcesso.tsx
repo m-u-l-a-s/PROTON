e7b9d00b6ea1f731f3d274a7c36b94874a265a0a
Alícea P. de Lima Rocha <alicealrocha@outlook.com>
2023-10-31 21:23:35 -0300

    Revert "Merge branch 'main' of https://github.com/m-u-l-a-s/PROTON"
    This reverts commit 20ac7b37ba2b93818873d632ca1c615223ad11a2, reversing
    changes made to 2a5dcc35fa4d3908ac7e67aa7b9705d3ee8138a1.

--- api_proton/src/pages/myProjects/CardProcesso.tsx
+++ api_proton/src/pages/myProjects/CardProcesso.tsx
@@ -26,90 +26,22 @@ export const CardProcesso = (prop: any) => {
     }, []);
 
 
-    const verificaStatus = async (processId: any) => {
-        try {
-        // Buscar todas as etapas do processo:
-        const etapasResponse = await fetch(
-            `http://localhost:5000/get_etapa_by_processo/${processId}`
-        );
-        if (!etapasResponse.ok) {
-            throw new Error("Não foi possível buscar etapas.");
-        }
+    const verificaStatus = () => {
+        return "CN";
+    };
 
-        // Converter resposta em JSON:
-        const etapas = await etapasResponse.json();
-
-        let temAnexos = false;
-        let temAnexosEmAtraso = false;
-        let todasEtapasValidadas = false;
-        let etapasValidadas = 0;
-
-        for (const etapa of etapas) {
-            const anexosResponse = await fetch(
-                `http://localhost:5000/get_anexos_by_etapa/${etapa.etapa_id}`
-            );
-            if (!anexosResponse.ok) {
-                throw new Error("Erro ao buscar anexos.");
-            }
-            const anexosData = await anexosResponse.json();
-
-            if (anexosData.contador !== "0") {
-                temAnexos = true;
-            }
-
-            const etapaDataConclusao = new Date(etapa.etapa_data_conclusao);
-            const hoje = new Date();
-
-            if (etapaDataConclusao < hoje) {
-                temAnexosEmAtraso = true;
-            } else {
-            }
-
-            if (etapa.etapa_status === "C") {
-                etapasValidadas++;
-            }
-
-            if (etapas.length <= etapasValidadas) {
-                todasEtapasValidadas = true;
-            }
-        }
-
-        if (todasEtapasValidadas) {
-            return "CN";
-            }
-        if (temAnexosEmAtraso) {
-            return "AT";
-        }
-        if (temAnexos) {
-            return "EA";
-        }
-        return "NI";
-    } catch (error) {
-        console.error("Error in verificaStatus:", error);
-        return "NI";
-    }
-};
-
-export const CardProcesso = (prop: any) => {
-    const [status, setStatus] = useState("NI");
     const navigate = useNavigate();
 
     const handleClick = () => {
         navigate("/visualizarProjeto", { state: { id: prop.processoID } });
     };
 
-    useEffect(() => {
-        // Fetch and set the status when the component mounts
-        verificaStatus(prop.processoID).then((result) => {
-            setStatus(result);
-        });
-    }, [prop.processoID]);
-
     return (
         <div onClick={handleClick} className="cardProcesso">
-            <BarraPrioridade status={status} />
-            <div className="card">
+            <BarraPrioridade status={verificaStatus()} />
+            <div className="card" style={{ fontFamily: 'poppins', fontSize: '0.8em' }}>
                 <h2>{prop.name}</h2>
+                {ContarEtapasP}
                 <h3>{prop.resp}</h3>
             </div>
         </div>
