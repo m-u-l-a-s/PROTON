9d08a9e83b7056b3caa3805b971b67d358a067a7
AlitaAmancio <amancioalita@gmail.com>
2023-11-05 21:36:05 -0300

    Adicionando barra de status no card de etapa

---
+++ api_proton/src/pages/novoProjeto/BarraStatus.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./BarraStatus.css";
+
+const BarraPrioridade = (props: { status: string }) => {
+    const navigate = useNavigate();
+    const [backgroundColor, setBackgroundColor] = useState<string>("");
+
+    useEffect(() => {
+
+        const P = "Pendente";
+        const A = "Em aprovação"
+        const C = "Concluído"
+
+        switch (props.status) {
+            case A:
+                setBackgroundColor("#FFD15C");
+                break;
+            case P:
+                setBackgroundColor("#FF7058");
+                break;
+            case C:
+                setBackgroundColor("#5DC474");
+                break;
+            default:
+                setBackgroundColor("transparent");
+        }
+    }, [props.status]);
+
+    return <div className="barraStatus" style={{ backgroundColor }}></div>;
+};
+
+export default BarraPrioridade;
--- api_proton/src/pages/novoProjeto/Steps.tsx
+++ api_proton/src/pages/novoProjeto/Steps.tsx
@@ -2,6 +2,8 @@ import "./Steps.css"
 import { useNavigate } from 'react-router-dom'
 import { Icon } from "@mui/material";
 import AttachmentIcon from "@mui/icons-material/Attachment";
+import BarraStatus from "./BarraStatus";
+import { useState } from "react";
 
 export const Steps = (prop: any) => {
     const navigate = useNavigate();
@@ -12,13 +14,16 @@ export const Steps = (prop: any) => {
 
     return (
         <div onClick={handleClick} className="cardEtapa">
+            <div>
+                <BarraStatus status={prop.status}></BarraStatus>
+            </div>
             <div className="top-row">
-                <div className="etapa" style={{fontSize:'1.1em'}}>{prop.nEtapa}</div>
-                <div className="pendente" style={{fontSize:'1em'}}>{prop.status}</div>
+                <div className="etapa" style={{ fontSize: '1.1em' }}>{prop.nEtapa}</div>
+                <div className="pendente" style={{ fontSize: '1em' }}>{prop.status}</div>
             </div>
             <div className="bottom-row">
-                <div className="descricao" style={{fontSize:'1em'}}> {prop.desc}</div>
-                <div className="anexados" style={{fontSize:'0.9em'}}>
+                <div className="descricao" style={{ fontSize: '1em' }}> {prop.desc}</div>
+                <div className="anexados" style={{ fontSize: '0.9em' }}>
                     <Icon>
                         <AttachmentIcon />
                     </Icon>
