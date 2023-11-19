293a29d22b94b2e17e2a98c561eda390c1c9b027
anxjos <jonassouzafonseca@gmail.com>
2023-11-19 16:23:52 -0300

    Conexão com elephantSQL

--- server/db.js
+++ server/db.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
+const  dotenv = require('dotenv');
 
+dotenv.config();
+
+//Comentar esse trecho ao conectar com elephantSQL
 const pool = new Pool({
     host: "localhost",
     user: "postgres",
     port: 5432,
-    password: "Dev123",
+    password: "fatec",
     database: "proton",
 });
+
+//Descomentar esse trecho ao conectar com elephantSQL
+// const pool = new Pool({
+//     connectionString: process.env.URLCloud, 
+// });
 
 module.exports = pool;
