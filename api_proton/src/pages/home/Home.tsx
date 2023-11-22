aa9c63f5878dd2cc3f1562e1e0b6c8c97732c27b
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-11-22 16:18:26 -0300

    Revert "novo script do banco e personalização da home para Adm"
    This reverts commit bfc25125ca4a8f4302db8b4a8acad8d6f55fb264.

--- api_proton/src/pages/home/Home.tsx
+++ api_proton/src/pages/home/Home.tsx
@@ -69,9 +69,8 @@ export const Home = () => {
     const [nEtapasPendentes, setContarPendentes] = useState(0);
     const ContarPendentes = async () => {
         try {
-            const usuario_nivel = "CL" || "AD";
             const nivel = {
-            usuario_nivel: usuario_nivel
+                usuario_nivel: "CL",
             };
 
             const response = await fetch(
@@ -94,9 +93,8 @@ export const Home = () => {
     const [nEtapasConcluidas, setContarConcluidos] = useState(0);
     const ContarConcluidos = async () => {
         try {
-            const usuario_nivel = "CL" || "AD";
             const nivel = {
-            usuario_nivel: usuario_nivel
+                usuario_nivel: "CL",
             };
 
             const response = await fetch(
@@ -119,9 +117,8 @@ export const Home = () => {
     const [nEtapasEmAprocaçao, setContarAprovacao] = useState(0);
     const ContarAprovacao = async () => {
         try {
-            const usuario_nivel = "CL" || "AD";
             const nivel = {
-            usuario_nivel: usuario_nivel
+                usuario_nivel: "CL",
             };
 
             const response = await fetch(
@@ -144,9 +141,8 @@ export const Home = () => {
     const [nEtapasAtrasadas, setContarAtrasados] = useState(0);
     const ContarAtrasados = async () => {
         try {
-            const usuario_nivel = "CL" || "AD";
             const nivel = {
-            usuario_nivel: usuario_nivel
+                usuario_nivel: "CL",
             };
 
             const response = await fetch(
@@ -166,32 +162,28 @@ export const Home = () => {
     };
 
     // Etapas a Vencer
-const [nEtapasAVencer, setContarAVencer] = useState(0);
+    const [nEtapasAVencer, setContarAVencer] = useState(0);
+    const ContarAVencer = async () => {
+        try {
+            const nivel = {
+                usuario_nivel: "CL",
+            };
 
-const ContarAVencer = async () => {
-    try {
-        const usuario_nivel = "CL" || "AD";
-        const nivel = {
-            usuario_nivel: usuario_nivel
-        };
-
-        const response = await fetch(
-            `http://localhost:5000/contarEtapasAVencer/${
-                JSON.parse(perfil).usuario_id
-            }/${nivel.usuario_nivel}`
-        );
-
-        if (response.ok) {
-            const data = await response.json();
-            setContarAVencer(data.count);
-        } else {
-            console.error("Erro na resposta da solicitação:", response);
+            const response = await fetch(
+                `http://localhost:5000/contarEtapasAVencer/${
+                    JSON.parse(perfil).usuario_id
+                }/${JSON.parse(perfil).usuario_nivel}`
+            );
+            if (response.ok) {
+                const data = await response.json();
+                setContarAVencer(data.count);
+            } else {
+                console.error("Erro na resposta da solicitação:", response);
+            }
+        } catch (error) {
+            console.error("Erro ao buscar o número de etapas:", error);
         }
-    } catch (error) {
-        console.error("Erro ao buscar o número de etapas:", error);
-    }
-};
-
+    };
 
     //Gráfico
     // eslint-disable-next-line react-hooks/exhaustive-deps
