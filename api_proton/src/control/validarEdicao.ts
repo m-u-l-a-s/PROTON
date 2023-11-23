eb2883a190bee5e925ced9ce715672a2f7f30b7d
AmendoaM <delakyu@gmail.com>
2023-11-23 10:39:58 -0300

    Subindo alterações no perfil admin (botão)

--- api_proton/src/control/validarEdicao.ts
+++ api_proton/src/control/validarEdicao.ts
@@ -1,52 +1,36 @@
 export function validarEdicao(pagina: string, id: number) {
-    const usuarioData = puxaID();
-    
-    // Verifica se puxaID() retornou um valor válido
-    if (usuarioData && usuarioData.usuario_id !== null) {
-        const UsuarioID = usuarioData.usuario_id;
+  const isLoginPage =
+    window.location.pathname === "/login" || window.location.pathname === "/" || window.location.pathname === "";
 
-        switch (pagina) {
-            case 'DetalheEtapa':
-            case 'VisualizarProjeto':
-            case 'Anexos':
-                return !(UsuarioID === id);
+  if (isLoginPage) {
+    return false;
+  }
 
-            case 'MyProjects':
-                return !(usuarioData.usuario_nivel === 'LE');
+  let UsuarioID = puxaID().usuario_id;
+  switch (pagina) {
+    case "DetalheEtapa":
+    case "VisualizarProjeto":
+    case "Anexos":
+      return !(UsuarioID === id);
 
-            case 'MenuSuperior':
-                const isLoginPage = (window.location.pathname === "/login") || (window.location.pathname === "/");
-                return (isLoginPage ? false : (usuarioData.usuario_nivel === 'CL'));
+    case "MyProjects":
+      return !(puxaID().usuario_nivel === "LE");
 
-            default:
-                // Lógica padrão, caso a página não corresponda a nenhum dos casos
-                return false;
-        }
-    } else {
-        // Tratamento para caso puxaID() retorne null ou usuário_id seja null
-        return false;
-    }
+    case "MenuSuperior":
+      return isLoginPage ? false : puxaID().usuario_nivel === "CL";
+
+    case "btnCadastro":
+      return isLoginPage ? false : puxaID().usuario_nivel === "AD";
+
+    default:
+  }
 }
 
 const puxaID = () => {
-    try {
-        const perfilJSON: string | null = sessionStorage.getItem('perfil');
-
-        if (perfilJSON !== null) {
-            const parsedPerfil = JSON.parse(perfilJSON);
-
-            if (parsedPerfil && typeof parsedPerfil === 'object' && 'usuario_id' in parsedPerfil) {
-                return parsedPerfil;
-            } else {
-                // Se 'perfil' ou 'usuario_id' não existirem
-                return { usuario_id: null };
-            }
-        } else {
-            // Se 'perfil' for nulo
-            return { usuario_id: null };
-        }
-    } catch (error) {
-        console.error("Erro ao analisar o perfil JSON:", error);
-        return { usuario_id: null };
-    }
+  try {
+    const perfilJSON: any = sessionStorage.getItem("perfil");
+    return JSON.parse(JSON.parse(perfilJSON));
+  } catch (error) {
+    return "ER";
+  }
 };
--- api_proton/src/shared/components/menu_superior/MenuSuperior.tsx
+++ api_proton/src/shared/components/menu_superior/MenuSuperior.tsx
@@ -1,184 +1,173 @@
 import {
-    AppBar,
-    Box,
-    Button,
-    Container,
-    Grid,
-    Stack,
-    Toolbar,
-    useTheme,
-    InputLabel,
-    MenuItem,
-    Select,
-    IconButton,
-    Menu,
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Toolbar,
+  useTheme,
+  InputLabel,
+  MenuItem,
+  Select,
+  IconButton,
+  Menu,
 } from "@mui/material";
 import { MouseEvent, ReactNode } from "react";
 import Logo from "../img/Proton.png";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {
-    useEffectSession,
-    useSessionStorageOrDefault,
-} from "../../../control/useSessionStorage";
+import { useEffectSession, useSessionStorageOrDefault } from "../../../control/useSessionStorage";
 import { findAllByAltText } from "@testing-library/react";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { validarEdicao } from "../../../control/validarEdicao";
 import { BaseURL } from "../../../control/BaseURL";
 
 export const MenuSuperior: React.FC<{
-    children: ReactNode;
-    hideHome: boolean;
-    hideProcesses: boolean;
-    hideDocuments: boolean;
-    showCombo: boolean;
+  children: ReactNode;
+  hideHome: boolean;
+  hideProcesses: boolean;
+  hideDocuments: boolean;
+  showCombo: boolean;
 }> = ({ children, hideHome, hideProcesses, hideDocuments, showCombo }) => {
-    const theme = useTheme();
-    const [usuario, setUsuario] = useState([
-        {
-            usuario_id: 1,
-            usuario_nome: "Betrano",
-            usuario_senha: "senha123",
-            usuario_data_cadastro: new Date(),
-            usuario_nivel: "CL",
-            usuario_email: "betrano@gmail.com",
-        },
-        {
-            usuario_id: 2,
-            usuario_nome: "Fulano",
-            usuario_senha: "senha456",
-            usuario_data_cadastro: new Date(),
-            usuario_nivel: "LE",
-            usuario_email: "fulano@gmail.com",
-        },
-    ]);
+  const theme = useTheme();
+  const [usuario, setUsuario] = useState([
+    {
+      usuario_id: 1,
+      usuario_nome: "Betrano",
+      usuario_senha: "senha123",
+      usuario_data_cadastro: new Date(),
+      usuario_nivel: "CL",
+      usuario_email: "betrano@gmail.com",
+    },
+    {
+      usuario_id: 2,
+      usuario_nome: "Fulano",
+      usuario_senha: "senha456",
+      usuario_data_cadastro: new Date(),
+      usuario_nivel: "LE",
+      usuario_email: "fulano@gmail.com",
+    },
+  ]);
 
-    const [usuarioAtual, setUsuarioAtual] = useState(
-        useSessionStorageOrDefault("perfil", usuario[0])
-    );
-    useEffectSession("perfil", usuarioAtual);
+  const [usuarioAtual, setUsuarioAtual] = useState(useSessionStorageOrDefault("perfil", usuario[0]));
+  useEffectSession("perfil", usuarioAtual);
 
-    const salvaPerfil = (event: { target: { value: any } }) => {
-        setUsuarioAtual(event.target.value);
-        window.location.reload();
-    };
+  const salvaPerfil = (event: { target: { value: any } }) => {
+    setUsuarioAtual(event.target.value);
+    window.location.reload();
+  };
 
-    const get_usuario = async () => {
-        try {
-            const response = await fetch(`${BaseURL()}/get_usuario/`);
-            const jsonData = await response.json();
-            setUsuario(jsonData); // Update the state with fetched data
-        } catch (error: any) {
-            console.log(error.message);
-        }
-    };
-
-    useEffect(() => {
-        get_usuario();
-    }, []);
-
-    // funções para menu ao lado do perfil
-
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-    const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    let user
+  const get_usuario = async () => {
     try {
-        user = JSON.parse(usuarioAtual)
-    } catch (error) {
-        console.log(error)
+      const response = await fetch(`${BaseURL()}/get_usuario/`);
+      const jsonData = await response.json();
+      setUsuario(jsonData); // Update the state with fetched data
+    } catch (error: any) {
+      console.log(error.message);
     }
+  };
 
-      // função para verificar se o perfil é CL -> não aparecer botões de home e processo
-      const [validaEdicao, setValidaEdicao] = useState(validarEdicao('MenuSuperior',0));
+  useEffect(() => {
+    get_usuario();
+  }, []);
 
+  // funções para menu ao lado do perfil
 
-    return (
-        <>
-            <Box width="100%" height="0">
-                <AppBar position="fixed" color="secondary">
-                    <Toolbar>
-                        <Container maxWidth="xl">
-                            <Grid
-                                container
-                                direction="row"
-                                justifyContent="space-between"
-                                alignItems="center"
-                            >
-                                <Grid item>
-                                    <img src={Logo} width={120} alt="logo" />
-                                </Grid>
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-                                <Grid item style={{}}>
-                                    <Stack direction="row" spacing={4}>
-                                        <Button
-                                            variant="text"
-                                            component={Link}
-                                            to="/Home"
-                                            style={{
-                                                fontFamily: "poppins",
-                                                fontWeight: "bold",
-                                                fontSize: "1em",
-                                                justifyContent: "center",
-                                                marginLeft: "400px",
-                                            }}
-                                            //função que esconde o botão para o CL
-                                            sx={{display: validaEdicao ? "none" : "flex"}}
-                                        >
-                                            Home
-                                        </Button>
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-                                        <Button
-                                            variant="text"
-                                            component={Link}
-                                            to="/MyProjects"
-                                            style={{
-                                                fontFamily: "poppins",
-                                                fontWeight: "bold",
-                                                fontSize: "1em",
-                                            }}
-                                            //função que esconde o botão para o CL
-                                            sx={{display: validaEdicao ? "none" : "flex"}}
-                                        >
-                                            Processos
-                                        </Button>
-                                        <Button
-                                            variant="text"
-                                            component={Link}
-                                            to="/Cadastro"
-                                            style={{
-                                                fontFamily: "poppins",
-                                                fontWeight: "bold",
-                                                fontSize: "1em",
-                                            }}
-                                            //função que esconde o botão para o CL
-                                            sx={{display: validaEdicao ? "none" : "flex"}}
-                                        >
-                                           <span style={{padding:"10px"}}>Cadastrar</span>  usuário
-                                        </Button>
-                                    </Stack>
-                                </Grid>
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-                                <Grid item style={{ marginLeft: "auto" }}>
-                                    <span id="currentUserText"
-                                        style={{
-                                            marginRight: "20px",
-                                            fontFamily: "poppins",
-                                            fontSize: "1.1em"
-                                        }}
-                                    >
-                                        {user && (
-                                            <>Olá, {user.usuario_nome}</>
-                                        )}
-                                        
-                                        {/* <Select
+  let user;
+  try {
+    user = JSON.parse(usuarioAtual);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // função para verificar se o perfil é CL -> não aparecer botões de home e processo
+  const [validaEdicao, setValidaEdicao] = useState(validarEdicao("MenuSuperior", 0));
+  const [btnCadastro, setBtnCadastro] = useState(validarEdicao("btnCadastro", 0));
+
+  return (
+    <>
+      <Box width="100%" height="0">
+        <AppBar position="fixed" color="secondary">
+          <Toolbar>
+            <Container maxWidth="xl">
+              <Grid container direction="row" justifyContent="space-between" alignItems="center">
+                <Grid item>
+                  <img src={Logo} width={120} alt="logo" />
+                </Grid>
+
+                <Grid item style={{}}>
+                  <Stack direction="row" spacing={4}>
+                    <Button
+                      variant="text"
+                      component={Link}
+                      to="/Home"
+                      style={{
+                        fontFamily: "poppins",
+                        fontWeight: "bold",
+                        fontSize: "1em",
+                        justifyContent: "center",
+                        marginLeft: "400px",
+                      }}
+                      //função que esconde o botão para o CL
+                      sx={{ display: validaEdicao ? "none" : "flex" }}
+                    >
+                      Home
+                    </Button>
+
+                    <Button
+                      variant="text"
+                      component={Link}
+                      to="/MyProjects"
+                      style={{
+                        fontFamily: "poppins",
+                        fontWeight: "bold",
+                        fontSize: "1em",
+                      }}
+                      //função que esconde o botão para o CL
+                      sx={{ display: validaEdicao ? "none" : "flex" }}
+                    >
+                      Processos
+                    </Button>
+                    <Button
+                      variant="text"
+                      component={Link}
+                      to="/Cadastro"
+                      style={{
+                        fontFamily: "poppins",
+                        fontWeight: "bold",
+                        fontSize: "1em",
+                      }}
+                      //função que esconde o botão para o CL
+                      sx={{ display: btnCadastro ? "flex" : "none" }}
+                    >
+                      <span style={{ padding: "10px" }}>Cadastrar</span> usuário
+                    </Button>
+                  </Stack>
+                </Grid>
+
+                <Grid item style={{ marginLeft: "auto" }}>
+                  <span
+                    id="currentUserText"
+                    style={{
+                      marginRight: "20px",
+                      fontFamily: "poppins",
+                      fontSize: "1.1em",
+                    }}
+                  >
+                    {user && <>Olá, {user.usuario_nome}</>}
+
+                    {/* <Select
                                             style={{
                                                 color: "white",
                                                 fontFamily: "poppins",
@@ -205,38 +194,25 @@ export const MenuSuperior: React.FC<{
                                             ))}
 
                                         </Select> */}
-                                    </span>
-                
-                                    <IconButton
-                                        aria-label="more"
-                                        id="long-button"
-                                        style={{ color: "white" }}
-                                        onClick={handleClick}
-                                    >
-                                        <MoreVertIcon />
-                                    </IconButton>
+                  </span>
 
-                                    <Menu
-                                        id="long-menu"
-                                        anchorEl={anchorEl}
-                                        open={Boolean(anchorEl)}
-                                        onClose={handleClose}
-                                    >
-                                        <MenuItem style={{ color: "white", fontFamily: "poppins" }}>
-                                            Logout
-                                        </MenuItem>
-                                    </Menu>
+                  <IconButton aria-label="more" id="long-button" style={{ color: "white" }} onClick={handleClick}>
+                    <MoreVertIcon />
+                  </IconButton>
 
-                                </Grid>
-                            </Grid>
-                        </Container>
-                    </Toolbar>
-                </AppBar>
-            </Box>
+                  <Menu id="long-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+                    <MenuItem style={{ color: "white", fontFamily: "poppins" }}>Logout</MenuItem>
+                  </Menu>
+                </Grid>
+              </Grid>
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </Box>
 
-            <Box width="100vw" marginTop={theme.spacing(8)}>
-                {children}
-            </Box>
-        </>
-    );
+      <Box width="100vw" marginTop={theme.spacing(8)}>
+        {children}
+      </Box>
+    </>
+  );
 };
